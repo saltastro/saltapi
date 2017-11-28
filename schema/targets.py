10448ccd55f09ffa8f3615cf3c0187bf4aaa6de3
nhlavutelo <nhlavutelo@saao.ac.za>
2017-11-28 12:09:40 +0200

    adding a target schema

--- schema/targets.py
+++ schema/targets.py
@@ -1,1 +1,24 @@
-# Target info
+from graphene import ObjectType, String, ID, Float, Field
+
+
+class Coordinates(ObjectType):
+    dec = Float()
+    ra = Float()
+
+
+class Horizon(ObjectType):
+    dec = Float()
+    ra = Float()
+
+
+class Magnitudes(ObjectType):
+    minimum = Float()
+    maximum = Float()
+    filter_name = String()  # from bandpass
+
+
+class Target(ObjectType):
+    id = ID()
+    name = String()
+    coordinates = Field(Coordinates)
+    magnitudes = Field(Magnitudes)
