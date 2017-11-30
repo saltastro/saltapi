971ec6d2f7eceec931b4b67ba709c7e33f6c1d00
nhlavutelo <nhlavutelo@saao.ac.za>
2017-11-30 12:28:29 +0200

    always a single target per proposal bug fixed

--- data/targets.py
+++ data/targets.py
@@ -1,10 +1,14 @@
 from schema.target import Target, Coordinates
 from schema.proposals import Proposal
-from schema.proposals import Proposal
 import pandas as pd
 from data import sdb_connect
 
+
 def target(row):
+    """    
+    :param row: a target query row (A pandas Series)
+    :return: Target Object (mapped only data need by TAC process)
+    """
     sign = -1 if row['DecSign'] == '-' else 1
     return Target(
             id="Target: " + str(row['Target_Id']),
@@ -19,20 +23,23 @@ def target(row):
 
 def get_targets(ids=None, proposals=None, semester=None, partner_code=None, proposal_code=None):
     """
-        If you do not provide ids you must provide semester or vise versa. value error will be raised if none is provide.
+        If you do not provide ids you must provide semester or vise versa. value error will be raised if none is 
+            provide.
         If both semester and ids are provide value error is raised.
-        the idea was to get a list of target not depending on any proposals or put targets to respective 
-        proposal but not both at the same time
-    :param ids: 
-    :param proposals: 
-    :param semester: 
-    :param partner_code: 
-    :param proposal_code: 
-    :return: 
+            The idea is to get a list of target not depending on any proposals or put targets to respective 
+            proposal but not both at the same time
+        
+    :param ids: Ids need to be provided by Proposals class (if you need a list of targets in a proposal) 
+                                        ==> Todo need to be moved 
+    :param proposals: Dict of proposals with they code as keys (if you need a list of targets in a proposal)
+    :param semester: semester querying for (if you need a list of targets)
+    :param partner_code: partner querying for (if you need a list of targets)
+    :param proposal_code: proposal code querying for (if you need a list of targets)
+    :return: list of targets (used by graphQL query) (if you need a list of targets)
     """
     if ids is not None and semester is not None:
         raise ValueError("targets are acquired by either ids or semester but not both")
-    targets = {}
+    targets = []
     if ids is None:
         if semester is None:
             raise ValueError("semester must be provided when query for Targets")
@@ -54,8 +61,8 @@ def get_targets(ids=None, proposals=None, semester=None, partner_code=None, prop
     conn.close()
     for i, row in results.iterrows():
         if proposals is None:
-            targets[row["Proposal_Code"]] = target(row)
+            targets.append(target(row))
         else:
             proposals[row["Proposal_Code"]].targets.append(target(row))
 
-    return targets.values()
+    return targets
