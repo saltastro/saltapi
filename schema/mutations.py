ada24561458234b798703e19af713ce36a49db7e
Eb-Zeero <macebelene@gmail.com>
2018-01-23 12:19:34 +0200

    mutation endpoint for tech comment and Liaison SA

--- schema/mutations.py
+++ schema/mutations.py
@@ -1,6 +1,9 @@
 import graphene
 from schema.time_allocations import UpdateTimeAllocations
+from schema.technical_review import UpdateTechComments, UpdateLiaisonAstronomer
 
 
 class Mutations(graphene.ObjectType):
     update_time_allocations = UpdateTimeAllocations.Field()
+    update_tech_comments = UpdateTechComments.Field()
+    update_liaison_astronomer = UpdateLiaisonAstronomer.Field()
