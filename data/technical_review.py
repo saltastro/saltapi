626096e49535f482b189c18c5b94d593ea637603
Christian Hettlage <hettlage@saao.ac.za>
2018-02-08 17:30:28 +0200

    Removed obsolete function

--- data/technical_review.py
+++ data/technical_review.py
@@ -29,46 +29,6 @@ def update_technical_reports(semester, reports):
         connection.close()
 
 
-def update_technical_report(proposal_code, semester, report, cursor):
-    """
-    Update a proposal's technical report.
-
-    If no entry exists for the proposal and semester, a new entry is created with the logged in
-    user as reporting astronomer. If an entry exists, its TechReport column value is updated,
-    but the reporting user is left unchanged.
-
-    This function executes the insert statement on the given cursor, but it does not explicitly
-    commit. You might have to commit the transaction in a function using this one.
-
-    Parameters
-    ----------
-    proposal_code : str
-        Proposal code of the proposal.
-    semester : str
-        Semester, such as '2017-2; or '2018-1'.
-    report : str
-        Technical report.
-    cursor: database cursor
-        Cursor on which the database command is executed.
-
-    Returns
-    -------
-    void
-    """
-
-    if not g.user.may_perform(Action.UPDATE_TECHNICAL_REPORT, proposal_code=proposal_code):
-        raise Exception('You are not allowed to update the technical report of proposal {proposal_code}'
-                        .format(proposal_code=proposal_code))
-
-    year, sem = semester.split('-')
-    sql = '''INSERT INTO ProposalTechReport (ProposalCode_Id, Semester_Id, Astronomer_Id, TechReport)
-                    SELECT pc.ProposalCode_Id, s.Semester_Id, u.Investigator_Id, %s
-                           FROM ProposalCode AS pc, Semester AS s, PiptUser AS u
-                           WHERE pc.Proposal_Code=%s AND (s.Year=%s AND s.Semester=%s) AND  u.Username=%s
-                    ON DUPLICATE KEY UPDATE TechReport=%s'''
-    cursor.execute(sql, (report, proposal_code, year, sem, g.user.username, report))
-
-
 def update_liaison_astronomers(assignments):
     """
     Update the database with a list of time allocations.
