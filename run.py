d9b4d5d8fd8cac1683c725375e3109b2b4997488
Christian Hettlage <hettlage@saao.ac.za>
2018-03-22 12:45:22 +0200

    Fixed permission issue

--- run.py
+++ run.py
@@ -142,7 +142,7 @@ def proposal_summary():
     semester = data['semester']
 
     # check permission
-    if not g.user.may_perform(Action.VIEW_PROPOSAL, proposal_code='2018-1-SCI-005'):
+    if not g.user.may_perform(Action.VIEW_PROPOSAL, proposal_code=proposal_code):
         raise InvalidUsage(message='You are not allowed to view the pdf summary of proposal {proposal_code}'
                            .format(proposal_code=proposal_code),
                            status_code=403)
@@ -159,7 +159,7 @@ def proposal_summaries():
 
     # check permission
     for proposal_code in proposal_codes:
-        if not g.user.may_perform(Action.VIEW_PROPOSAL, proposal_code='2018-1-SCI-005'):
+        if not g.user.may_perform(Action.VIEW_PROPOSAL, proposal_code=proposal_code):
             raise InvalidUsage(message='You are not allowed to view the pdf summary of proposal {proposal_code}'
                                .format(proposal_code=proposal_code),
                                status_code=403)
