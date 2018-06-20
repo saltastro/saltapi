63ef9bdb06356de3d61e8f6dcde0a8df4af3eea2
Eb-Zeero <macebelene@gmail.com>
2018-06-20 16:17:48 +0200

    technical_report

--- run.py
+++ run.py
@@ -192,10 +192,10 @@ def proposal_summary():
     data = request.json
     proposal_code = data['proposalCode']
     semester = data['semester']
+    partner = data['partner']
 
     # TODO: check permission
     if not g.user.may_perform(Action.DOWNLOAD_SUMMARY, proposal_code=proposal_code, partner=partner):
-
         raise InvalidUsage(message='You are not allowed to view the pdf summary of proposal {proposal_code}'
                            .format(proposal_code=proposal_code),
                            status_code=403)
@@ -213,7 +213,7 @@ def proposal_summaries():
 
     # check permission
     for proposal_code in proposal_codes:
-        if not g.user.may_perform(Action.VIEW_PROPOSAL, proposal_code=proposal_code):
+        if not g.user.may_perform(Action.DOWNLOAD_SUMMARY, proposal_code=proposal_code, partner=partner):
             raise InvalidUsage(message='You are not allowed to view the pdf summary of proposal {proposal_code}'
                                .format(proposal_code=proposal_code),
                                status_code=403)
