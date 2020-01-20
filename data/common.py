12c393dfc8817b45f916ddfa0279dbdb9560c959
Eb-Zeero <macebelene@gmail.com>
2020-01-20 13:28:30 +0200

    query prop id's for stats

--- data/common.py
+++ data/common.py
@@ -52,6 +52,46 @@ GROUP BY ProposalCode_Id, Semester_Id HAVING Semester = "{semester}"
     }
 
 
+def proposal_ids_for_statistics(semester, partner_code=None):
+    conn = sdb_connect()
+    all_partners = [p['Partner_Code'] for i, p in pd.read_sql("SELECT Partner_Code FROM Partner", conn).iterrows()]
+    conn.close()
+
+    sql = """
+SELECT distinct
+    Partner.Partner_Code AS PartnerCode,
+    ProposalCode_Id,
+    Proposal_Code,
+    Surname,
+    ProposalStatus_Id ,
+    CONCAT(Year, '-', Semester) AS Semester
+FROM ProposalCode
+    JOIN ProposalGeneralInfo USING(ProposalCode_Id)
+    JOIN MultiPartner USING(ProposalCode_Id)
+    JOIN ProposalContact USING(ProposalCode_Id)
+    JOIN Investigator ON (Leader_Id=Investigator_Id)
+    JOIN Semester USING(Semester_Id)
+    JOIN Partner ON (MultiPartner.Partner_Id = Partner.Partner_Id)
+GROUP BY ProposalCode_Id, Semester_Id HAVING Semester = "{semester}"
+    AND ProposalStatus_Id NOT IN (9, 3)
+    """.format(semester=semester)  # status 9 => Deleted, 3 => Rejected
+
+    conn = sdb_connect()
+
+    if partner_code is not None:
+        sql += """  AND PartnerCode IN ("{partner_codes}")
+                    """.format(partner_codes='", "'.join([partner_code]))
+    else:
+        sql += """  AND PartnerCode IN ("{partner_codes}")
+            """.format(partner_codes='", "'.join(all_partners))
+
+    proposal_code_ids = []
+    for index, r in pd.read_sql(sql, conn).iterrows():
+        proposal_code_ids.append(str(r['ProposalCode_Id']))
+    conn.close()
+    return proposal_code_ids
+
+
 def sql_list_string(values):
     """
     Generate a string for a list to use with the MySQL IN operator.
